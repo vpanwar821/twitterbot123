ea5a3605c88e3193688d6c565b4580831f11b7bf
unknown <vikaspanwar16@gmail.com>
2017-09-16 20:39:38 +0530

    first commit

---
+++ bot.js
@@ -0,0 +1,71 @@
+var twit = require('twit');
+var config = require('./config.js');
+
+var retweet=function(){
+    var params={
+        q:'#nodejs',
+        result_type:'recent',
+        lang:'en'
+    }
+
+
+Twitter.get('search/tweets',params,function(err,data){
+    if(!err){
+        var retweetId=data.statuses[0].id_str;
+
+        Twitter.post('statuses/retweet/:id',{
+                id:retweetId
+        },function(err,response){
+            if(response)
+            {
+                console.log('Retweeted!!')
+            }
+            if(err)
+            {
+               console.log('Something went wrong while Retweeting!!') 
+            }
+
+        });
+    }
+    else{
+        console.log('Something went wrong while searching..')
+    }
+ });
+}
+retweet();
+setInterval(retweet,3000000); 
+
+var favoriteTweet=function(){
+    var params={
+        q:'#nodejs',
+        result_type:'recent',
+        lang:'en'
+        }
+
+        Twitter.get('search/tweets',params,function(err,data){
+            var tweet=data.statuses;
+            var randomTweet=ranDom(tweet);
+            
+            if(typeof randomTweet!='undefined')
+            {
+                Twitter.post('favorites/create',{id:randomTweet.id_str},function(err,response){
+                    if(err)
+                    {
+                        console.log('Cannot be favorite');
+                    }
+                    else{
+                        console.log('Favorite Success!!');
+                    }
+                });
+            }
+        });
+    }
+    favoriteTweet();
+
+    setInterval(favoriteTweet,3600000);
+
+    function ranDom(arr){
+        var index=Math.floor(Math.random()*arr.length);
+        return arr[index];
+    };
+    
---
+++ config.js
@@ -0,0 +1,9 @@
+var twit = require('twit');
+module.exports={
+consumer_key:'Kdp0NQFlOeHTKV4gTkxinsEnO',
+consumer_secret:'ivymS8GsuhXjLZ4bdAEnXxYgqU7SGij83YJsxzRScsqR3xr3FV',
+access_token:'909002939498250240-XrnHhfkM4nb5yLMVDQhThD4Aa0Avq4c',
+access_token_secret:'SpkuC2iCverxIvU5swek3gf9AnvxwfTsJgZxevrOubyd9'
+}
+
+var Twitter = new twit(config);
